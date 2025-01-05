e6196f9f51c6a6c31b44cc89c5aa50ee0b09a9d2
Chenjingzhi-cjb <1286576927@qq.com>
2025-01-05 15:31:45 +0800

    Update MseedProcess PyScript
    The structure of the entire program was changed from "converting data to string format first and then taking out the data for calculation" to "taking out the data for calculation directly". The calculations in the frequency domain were also optimized.

--- PyScript/main.py
+++ PyScript/main.py
@@ -7,73 +7,36 @@ import xlsxwriter
 from obspy.core import read
 
 
-def format_conversion(folder_path):
-    count = 0
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".mseed"):
-            file_path = os.path.join(folder_path, filename)
-
-            st = read(file_path)
-            trace_num = len(st)
-
-            if trace_num == 1:
-                st.write(file_path + '_TSPAIR', format='TSPAIR')
-                count = count + 1
-                print('%04d --------> %s' % (count, filename))
-            else:
-                print('ERROR:the number of the trace is not one ??? %s' % filename)
-
-
-def data_alignment(folder_path, count):
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".mseed_TSPAIR"):
-            file_path = os.path.join(folder_path, filename)
-
-            with open(file_path, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
-
-            if len(lines) > count:
-                lines = lines[:count]
-
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.writelines(lines)
-
-
-def data_conversion_base(filename, folder_path):
+def data_conversion_base(filename, folder_path, alignment_count):
     file_path = os.path.join(folder_path, filename)
 
-    with open(file_path, 'r') as f:
-        raw_datas = f.readlines()
-        datas = [int(i.split(' ')[-1]) for i in raw_datas[1:]]
+    stream = read(file_path)
+    trace = stream[0]
 
     # 输入信号
-    x = np.array(datas)
+    x = np.array(trace.data)
+    if len(x) > alignment_count:
+        x = x[:alignment_count]
 
     # 获取采样率（Fs）的值
-    fs = 1000  # 假设采样率为 1000 Hz
-
-    # 计算频率步长
-    freq_step = fs / len(x)
+    fs = trace.stats.sampling_rate
 
     # 创建频率轴
-    freq_axis = np.arange(0, fs / 2, freq_step)
-
-    # 零填充输入信号以匹配频率轴的长度
-    padded_x = np.pad(x, (0, len(freq_axis) * 2 - len(x)), mode='constant')
+    freq_axis = np.fft.rfftfreq(len(x), d=1/fs)
 
     # 计算傅里叶变换
-    fx = np.fft.fft(padded_x)
+    fx = np.fft.rfft(x)
 
     # 取前半部分复数的绝对值
-    half_abs_fx = abs(fx[:len(freq_axis)])
+    half_abs_fx = abs(fx)
 
     return freq_axis, half_abs_fx
 
 
-def data_conversion_C(folder_path):
+def data_conversion_C(folder_path, alignment_count):
     for index, filename in enumerate(os.listdir(folder_path)):
-        if filename.endswith(".mseed_TSPAIR"):
-            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path)
+        if filename.endswith(".mseed"):
+            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_count)
 
             # 绘制散点图并将图像存储到列表中
             plt.figure(index)
@@ -86,14 +49,14 @@ def data_conversion_C(folder_path):
     plt.show()
 
 
-def data_conversion_S(folder_path):
+def data_conversion_S(folder_path, alignment_count):
     workbook = xlsxwriter.Workbook(folder_path + '\\' + "result.xlsx")
     worksheet = workbook.add_worksheet()
 
     col = 0
     for filename in os.listdir(folder_path):
-        if filename.endswith(".mseed_TSPAIR"):
-            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path)
+        if filename.endswith(".mseed"):
+            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_count)
 
             row = 0
             for i in range(len(freq_axis)):
@@ -108,13 +71,10 @@ def data_conversion_S(folder_path):
 
 
 def main(folder_path, alignment_count, exec_type):
-    format_conversion(folder_path)
-    data_alignment(folder_path, alignment_count)
-
     if exec_type == 'C':
-        data_conversion_C(folder_path)
+        data_conversion_C(folder_path, alignment_count)
     elif exec_type == 'S':
-        data_conversion_S(folder_path)
+        data_conversion_S(folder_path, alignment_count)
 
     print("Finished!")
 
