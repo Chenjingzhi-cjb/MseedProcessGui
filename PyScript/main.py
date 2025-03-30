36a35a5ae86db45f99a0a0eb65a44471fe9071f2
Chenjingzhi-cjb <1286576927@qq.com>
2025-03-30 16:56:17 +0800

    Added "fs" parameter to control which files should be processed.

--- PyScript/main.py
+++ PyScript/main.py
@@ -36,21 +36,26 @@ def data_conversion_base(filename, folder_path, alignment_time):
     return freq_axis, half_abs_fx
 
 
-def data_conv_pyplot(folder_path, alignment_time, cmd):
-    for index, filename in enumerate(os.listdir(folder_path)):
-        if filename.endswith(".mseed"):
-            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_time)
+def data_conv_pyplot(folder_path, alignment_time, cmd, files_name):
+    if files_name is None:
+        files_name = []
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".mseed"):
+                files_name.append(filename)
 
-            # 绘制散点图并将图像存储到列表中
-            plt.figure(index)
-            plt.plot(freq_axis[1:], half_abs_fx[1:], linestyle='-')
-            plt.xlabel('Frequency (Hz)')
-            plt.ylabel('Magnitude')
-            plt.title('FFT Spectrum for {}'.format(filename))  # 使用文件名作为标题
+    for index, filename in enumerate(files_name):
+        freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_time)
 
-            if cmd == "save":
-                # 保存图像
-                plt.savefig(f"{folder_path}/{filename.removesuffix('.mseed')}.png")
+        # 绘制散点图并将图像存储到列表中
+        plt.figure(index)
+        plt.plot(freq_axis[1:], half_abs_fx[1:], linestyle='-')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Magnitude')
+        plt.title('FFT Spectrum for {}'.format(filename))  # 使用文件名作为标题
+
+        if cmd == "save":
+            # 保存图像
+            plt.savefig(f"{folder_path}/{filename.removesuffix('.mseed')}.png")
 
     if cmd == "show":
         # 显示所有图像
@@ -58,7 +63,7 @@ def data_conv_pyplot(folder_path, alignment_time, cmd):
 
 
 def num_to_excel_col(n):
-    """将 0-based 列索引转换为 Excel 列名（A, B, C, ..., Z, AA, AB, ...）"""
+    """将 0-based 列索引转换为 Excel 列名 (A, B, C, ..., Z, AA, AB, ...) """
     col_name = ""
     while n >= 0:
         col_name = chr(n % 26 + ord('A')) + col_name
@@ -66,40 +71,45 @@ def num_to_excel_col(n):
     return col_name
 
 
-def data_conv_excel(folder_path, alignment_time):
+def data_conv_excel(folder_path, alignment_time, files_name):
+    if files_name is None:
+        files_name = []
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".mseed"):
+                files_name.append(filename)
+
     workbook = xlsxwriter.Workbook(folder_path + '\\' + "result.xlsx")
     worksheet = workbook.add_worksheet()
 
     chart = workbook.add_chart({"type": "scatter", 'subtype': 'smooth'})
 
     col = 0
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".mseed"):
-            # 计算频域数据
-            freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_time)
+    for filename in files_name:
+        # 计算频域数据
+        freq_axis, half_abs_fx = data_conversion_base(filename, folder_path, alignment_time)
 
-            # 写入频域数据
-            worksheet.write(0, col, filename.removesuffix(".mseed"))
+        # 写入频域数据
+        worksheet.write(0, col, filename.removesuffix(".mseed"))
 
-            row = 1  # 去直流
-            for i in range(1, len(freq_axis)):
-                worksheet.write(row, col, freq_axis[i])
-                worksheet.write(row, col + 1, half_abs_fx[i])
+        row = 1  # 去直流
+        for i in range(1, len(freq_axis)):
+            worksheet.write(row, col, freq_axis[i])
+            worksheet.write(row, col + 1, half_abs_fx[i])
 
-                row += 1
+            row += 1
 
-            # 添加数据到图表
-            x_col = num_to_excel_col(col)
-            y_col = num_to_excel_col(col + 1)
+        # 添加数据到图表
+        x_col = num_to_excel_col(col)
+        y_col = num_to_excel_col(col + 1)
 
-            chart.add_series({
-                "name": f"={worksheet.name}!${x_col}$1",  # 组名称
-                "categories": f"={worksheet.name}!${x_col}$2:${x_col}${len(freq_axis)}",  # X 轴数据
-                "values": f"={worksheet.name}!${y_col}$2:${y_col}${len(freq_axis)}",  # Y 轴数据
-                "line": {"width": 1.5, "smooth": True},  # 平滑曲线
-            })
+        chart.add_series({
+            "name": f"={worksheet.name}!${x_col}$1",  # 组名称
+            "categories": f"={worksheet.name}!${x_col}$2:${x_col}${len(freq_axis)}",  # X 轴数据
+            "values": f"={worksheet.name}!${y_col}$2:${y_col}${len(freq_axis)}",  # Y 轴数据
+            "line": {"width": 1.5, "smooth": True},  # 平滑曲线
+        })
 
-            col += 2
+        col += 2
 
     # 设置图表属性
     chart.set_title({"name": "Spectral Analysis"})
@@ -122,13 +132,13 @@ def data_conv_excel(folder_path, alignment_time):
     workbook.close()
 
 
-def main(folder_path, alignment_time, exec_type):
+def main(folder_path, alignment_time, exec_type, files_name):
     if exec_type == 'Plot1':
-        data_conv_pyplot(folder_path, alignment_time, "show")
+        data_conv_pyplot(folder_path, alignment_time, "show", files_name)
     elif exec_type == 'Plot2':
-        data_conv_pyplot(folder_path, alignment_time, "save")
+        data_conv_pyplot(folder_path, alignment_time, "save", files_name)
     elif exec_type == 'Excel':
-        data_conv_excel(folder_path, alignment_time)
+        data_conv_excel(folder_path, alignment_time, files_name)
 
     print("Finished!")
 
@@ -138,7 +148,8 @@ if __name__ == '__main__':
     parser.add_argument('-F', help='Folder Path')
     parser.add_argument('-A', type=int, help='Alignment Time')
     parser.add_argument('-E', choices=['Plot1', 'Plot2', 'Excel'], help='Exec Type')
+    parser.add_argument('-fs', nargs='+', help='Files Name')
 
     args = parser.parse_args()
 
-    main(args.F, args.A, args.E)
+    main(args.F, args.A, args.E, args.fs)
